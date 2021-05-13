a72080d918af49d2ac690e995c3d49334741e242
Gemma <hello@gemma.dev>
2021-05-13 16:22:58 +0100

    strapi i18n demo

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6a1824cff66bdd16d24f264b766893e9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '323369cc-d27f-4963-9c81-78cf1ced568a'
+};
